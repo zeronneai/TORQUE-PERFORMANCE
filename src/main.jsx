74601a81b1a4f93f6101b652ce575f6b57c26e7a
zeronneai <zeronne.ai@gmail.com>
2026-03-25 16:34:39 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+import { ClerkProvider } from '@clerk/clerk-react';
+import { dark } from '@clerk/themes'; // <--- ESTA LÍNEA FALTA
 
+// ... resto del código
+<ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{ baseTheme: dark }}>
+  <App />
+</ClerkProvider>
 
 // Aquí pegas tu clave de Clerk (la que empieza con pk_test_)
 const PUBLISHABLE_KEY = "pk_test_YnJpZWYtcm9kZW50LTQ1LmNsZXJrLmFjY291bnRzLmRldiQ" 
