ac9a833ad46e6ea9ef9e5d03c6f9881223865f01
zeronneai <zeronne.ai@gmail.com>
2026-03-25 16:10:58 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+import { dark } from '@clerk/themes'
+
+// Aquí pegas tu clave de Clerk (la que empieza con pk_test_)
+const PUBLISHABLE_KEY = "pk_test_YnJpZWYtcm9kZW50LTQ1LmNsZXJrLmFjY291bnRzLmRldiQ" 
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{ baseTheme: dark }}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>,
 )
